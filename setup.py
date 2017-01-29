87b7521db1c87bf094b9351fbfa8e5d6f8b8e00f
Larz60+ <Larz60p@users.noreply.github.com>
2017-01-29 12:53:40 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='ShowInstalledPackages',
+  packages=['ShowInstalledPackages'], # this must be the same as the name above
+  version='0.1.1',
+  description='List all user installed packages and details',
+  author='Larz60+',
+  author_email='larry@dimplechad.com',
+  url='https://github.com/Larz60p/ShowInstalledPackages',
+  download_url='https://github.com/Larz60p/ShowInstalledPackages/tarball/0.1.1',
+  keywords=['add', 'sub', 'tests'],
+  classifiers=[]
+)
