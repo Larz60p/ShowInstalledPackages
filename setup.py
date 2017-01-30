fb31bb7539be34999200586261e4c89bed26b54f
Larz60+ <Larz60p@users.noreply.github.com>
2017-01-30 08:23:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
+import os
 from distutils.core import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-  name='ShowInstalledPackages',
-  packages=['ShowInstalledPackages'], # this must be the same as the name above
-  version='0.1.1',
-  description='List all user installed packages and details',
-  author='Larz60+',
-  author_email='larry@dimplechad.com',
-  url='https://github.com/Larz60p/ShowInstalledPackages',
-  download_url='https://github.com/Larz60p/ShowInstalledPackages/tarball/0.1.1',
-  keywords=['add', 'sub', 'tests'],
-  classifiers=[]
+    name='ShowInstalledPackages',
+    packages=['ShowInstalledPackages'],  # this must be the same as the name above
+    summary='List all user installed packages and details',
+    version='0.1.1',
+    author='Larry McCaig (Larz60+)',
+    author_email='larry@dimplechad.com',
+    long_description=read('README.md'),
+    license="MIT",
+    url='https://github.com/Larz60p/ShowInstalledPackages',
+    download_url='https://github.com/Larz60p/ShowInstalledPackages/tarball/0.1.1',
+    keywords='tools packages installed utilities',
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Topic :: Utilities',
+        'License :: MIT License',
+    ],
 )
